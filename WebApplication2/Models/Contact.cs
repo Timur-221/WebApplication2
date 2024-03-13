e9268830e79b816b034bb691be5cb22fc3a58aa0
212007 <212007@1305-013>
2024-03-13 18:35:35 +0300

    Добавьте файлы проекта.

---
+++ WebApplication2/Models/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Contact
+    {
+        [Display(Name = "Введите имя")]
+        [Required(ErrorMessage = "Вам нужно ввести имя")]
+        public string Name { get; set; }
+
+        [Display(Name = "Введите фамилию")]
+        [Required(ErrorMessage = "Вам нужно ввести фамилмю")]
+        public string SurName { get; set; }
+
+        [Display(Name = "Введите возраст")]
+        [Required(ErrorMessage = "Вам нужно ввести возраст")]
+        public int Age { get; set; }
+
+        [Display(Name = "Введите почту")]
+        [Required(ErrorMessage = "Вам нужно ввести почту")]
+        public string Email { get; set; }
+
+        [Display(Name = "Введите сообщение")]
+        [StringLength(30, ErrorMessage ="Текст менее 30 символов")]
+        [Required(ErrorMessage = "Вам нужно ввести сообщение")]
+        public string Message { get; set; }
+    }
+}
